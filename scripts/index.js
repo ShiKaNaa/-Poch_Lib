8952acf771a0f760cbb49a7a7be0f2a5e4afc560
Raphael Houdry <raphael-59200@hotmail.fr>
2022-12-19 17:13:45 +0100

    add a book and cancel a book buttons working, need to work on API calls now

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const newBookTitleSelector = document.querySelector(".h2");
 const divConstructor = document.createElement("div");
 const buttonConstructor = document.createElement("button");
 
+// function to create button "Ajouter un livre"
 const createButtonAddABook = () => {
     buttonConstructor.innerText = "Ajouter un livre";
     newBookTitleSelector.appendChild(divConstructor).appendChild(buttonConstructor).classList.add("button-add-book");
@@ -15,11 +16,45 @@ const searchForm = () => {
     addABookDivSelector.addEventListener("click", newForm);
 }
 
+// function to create a form for the Google Books API
 const newForm = () => {
     removeButtonAddABook();
-    const formForGoogleAPI = document.appendChild();
+    const cancelButton = document.createElement("button");
+    const divForForm = document.createElement("div");
+    cancelButton.innerText = "Annuler";
+    cancelButton.classList.add("button-cancel-search");
+    divForForm.id = "formId";
+    divForForm.innerHTML = '<form action="" method="get" class="form-example">' + 
+                                '<div class="title-of-book">' +
+                                '<label for="title">Titre du livre</label>'+
+                                '<input type="text" name="title" id="title-book" required>' +
+                                '</div>' +
+                                '<div class="author-of-book">' +
+                                '<label for="author">Auteur</label>' +
+                                '<input type="text" name="author" id="author-book" required>' +
+                                '</div>' +
+                                '<div class="search-button">' +
+                                '<input type="submit" value="Rechercher">' +
+                                '</div>' + 
+                                '</form>';
+    
+    newBookTitleSelector.appendChild(divForForm).appendChild(cancelButton);
+    removeForm();  
 }
 
+// function to go back to "Ajouter un livre"
+
+
+const removeForm = () => {
+    const formDivSelector = document.getElementById("formId");
+    const cancelButtonSelector = document.querySelector(".button-cancel-search");
+    cancelButtonSelector.addEventListener("click", () => {
+        formDivSelector.remove();
+        createButtonAddABook();
+    });
+ }
+
+// function to remove button "Ajouter un livre"
 const removeButtonAddABook = () => {
     addABookDivSelector.remove();
 }
@@ -27,3 +62,4 @@ const removeButtonAddABook = () => {
 const addABookDivSelector = document.querySelector(".button-add-book");
 
 searchForm();
+

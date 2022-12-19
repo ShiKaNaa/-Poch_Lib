f9a664070446ac1dddbe347db4fdbfd37ed03300
Raphael Houdry <raphael-59200@hotmail.fr>
2022-12-19 15:04:40 +0100

    added button for new book

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
-console.log("Hi")
+console.log("JS Loaded");
+
+const newBookTitleSelector = document.querySelector(".h2");
+const divConstructor = document.createElement("div");
+const buttonConstructor = document.createElement("button");
+
+const createButtonAddABook = () => {
+    buttonConstructor.innerText = "Ajouter un livre";
+    newBookTitleSelector.appendChild(divConstructor).appendChild(buttonConstructor).classList.add("button-add-book");
+}
+
+createButtonAddABook();
+
+const searchForm = () => {
+    addABookDivSelector.addEventListener("click", newForm);
+}
+
+const newForm = () => {
+    removeButtonAddABook();
+    const formForGoogleAPI = document.appendChild();
+}
+
+const removeButtonAddABook = () => {
+    addABookDivSelector.remove();
+}
+
+const addABookDivSelector = document.querySelector(".button-add-book");
+
+searchForm();
